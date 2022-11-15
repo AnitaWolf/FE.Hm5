8e2d5a69fcdcfa0c3646d9fe5df41f0eaf253406
Anita <anito4ka83@yandex.com>
2022-11-15 20:52:04 +0100

    newfile

---
+++ script.js
@@ -0,0 +1,15 @@
+/*let num =[]                        //________1
+function calc (num) {
+    for (let i = 0; i <= 10; i++) {
+     num. push (i)  
+    } return num
+} console.log (calc(num))*/
+
+
+/*function calc (a,b) {                  //________2
+    if (a-b>0){
+ return a
+    }
+return b
+}
+console.log(calc(2,8))*/
